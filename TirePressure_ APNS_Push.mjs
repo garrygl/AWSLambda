16792332cc1f3c45c02cf1a4e4143eb82d236133
Garry S Glaser <60247760+garrygl@users.noreply.github.com>
2022-12-18 14:41:01 -0800

    Add files via upload

---
+++ TirePressure_ APNS_Push.mjs
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+const adm = require('amazon-device-messaging-node');
+
+exports.handler = async (event) => {
+  // Set up the ADM provider
+  const provider = new adm.Provider({
+    clientId: 'YOUR_CLIENT_ID',
+    clientSecret: 'YOUR_CLIENT_SECRET',
+    region: 'YOUR_REGION'
+  });
+
+  // Set up the notification
+  const notification = new adm.Notification();
+  notification.expiresAfter = 3600; // Expires in 1 hour.
+  notification.data.title = "Alert";
+  notification.data.body = "Warning - Low Tire Pressure Detected";
+  notification.data.custom = {'messageFrom': 'Tire Pressure Monitor System'};
+  notification.consolidationKey = 'my_consolidation_key';
+
+  // Send the notification
+  const result = await provider.send(notification, 'DEVICE_TOKEN');
+
+  console.log(result);
+
+  return result;
+};
---
+++ TirePressure_ADM_Push.mjs
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+const apn = require('apn');
+
+exports.handler = async (event) => {
+  // Set up the APN provider
+  const provider = new apn.Provider({
+    certificate: 'path/to/certificate.pem',
+    key: 'path/to/key.pem',
+  });
+
+  // Set up the notification
+  const notification = new apn.Notification();
+  notification.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires in 1 hour.
+  notification.badge = 3;
+  notification.sound = "ping.aiff";
+  notification.alert = "Alert";
+  notification.payload = {'messageFrom': 'Tire Pressure Monitor System'};
+  notification.topic = 'Warning - Low Tire Pressure Detected';
+
+  // Send the notification
+  const result = await provider.send(notification, 'device_token');
+
+  console.log(result);
+
+  return result;
+};
