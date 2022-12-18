25cf878241b1591544b71569820a522a9818ebe8
Garry S Glaser <60247760+garrygl@users.noreply.github.com>
2022-12-18 14:15:12 -0800

    Add files via upload

---
+++ TirePressure_FCM_Push.mjs
@@ -0,0 +1,29 @@
+const request = require('request');
+
+exports.handler = function(event, context, callback) {
+  const serverKey = 'YOUR_FCM_SERVER_KEY';
+  const message = {
+    to: '/topics/all',
+    data: {
+      message: 'Warning - Low Tire Pressure Detected!'
+    }
+  };
+
+  request({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `key=${serverKey}`
+    },
+    body: JSON.stringify(message)
+  }, function(error, response, body) {
+    if (error) {
+      console.error(error);
+      callback(error);
+    } else {
+      console.log(response.statusCode, body);
+      callback(null, response.statusCode, body);
+    }
+  });
+};
