28a92a7018c45e2f065decc6cd7655fe2a0cf541
Garry S Glaser <60247760+garrygl@users.noreply.github.com>
2022-12-18 11:49:15 -0800

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event) => {
+  const sns = new AWS.SNS();
+  const params = {
+    Message: 'Warning - Low Tire Pressure Detected', 
+    PhoneNumber: '+831566xxxx',
+    Subject: 'Tire Pressure'
+  };
+  try {
+    const result = await sns.publish(params).promise();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
